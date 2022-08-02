0247c5ac72214caa6ac7a0119f1d16bb284e03b6
Elizabeth Kenyon <elizabeth.kenyon@shopify.com>
2022-08-02 11:40:52 -0500

    Checkout UI Extension
    This branch represents step 5 in the workshop
    
    Add validaiton to field
    
    Use BuyerJourneyIntercept to block navigation

--- extensions/resident-id/src/index.jsx
+++ extensions/resident-id/src/index.jsx
@@ -1,34 +1,71 @@
 import React, { useState } from 'react';
-import  {render, TextField } from '@shopify/checkout-ui-extensions-react';
-
+import { render, TextField, useMetafield, useApplyMetafieldsChange, useBuyerJourneyIntercept } from '@shopify/checkout-ui-extensions-react';
 render('Checkout::Dynamic::Render', () => <App />);
 
 function App() {
   const [error, setError] = useState(false);
+  const updateMetafield = useApplyMetafieldsChange();
+
+  const METAFIELD_NAMESPACE = 'RESIDENT_ID_APP';
+  const METAFIELD_KEY = 'resident_id';
+
   const residentIdState = useMetafield({
     namespace: METAFIELD_NAMESPACE,
     key: METAFIELD_KEY,
   });
 
-  handleFieldChange = (value) => {
-    updateMetafield(
-         {
-           type: 'updateMetafield',
-           namespace: METAFIELD_NAMESPACE,
-           key: METAFIELD_KEY,
-           valueType: 'string',
-           value: value
-         }
-       );
-   };
+  useBuyerJourneyIntercept(() => {
+    if (!validateResidentId(residentIdState.value)) {
+      return {
+        behavior: 'block',
+        reason: 'Form is not valid.',
+        // if a partner tries block checkout, then `perform()` does not get called and nothing happens
+        // acts like `behavior: allow` 
+        perform: () => showValidationUI()
+      }
+    } else {
+      setError(false);
+      return {
+        behavior: 'allow',
+      }
+    }
+  });
 
-    return (
-      <TextField 
-        label='Resident ID'
-        value={residentIdState?.value}
-        error={error? 'Please provide a valid ID': false}
-        onChange={handleFieldChange}
-         />
-      );
+  const showValidationUI = () => {
+    console.log('validation UI');
+    setError(true);
+  }
+
+  const handleFieldChange = (value) => {
+    if (error) {
+      let validId = validateResidentId(value);
+      if (validId) {
+        setError(false);
+      }
+    }
+    updateMetafield(
+      {
+        type: 'updateMetafield',
+        namespace: METAFIELD_NAMESPACE,
+        key: METAFIELD_KEY,
+        valueType: 'string',
+        value: value
+      }
+    );
+  };
+
+
+  const validateResidentId = (value) => {
+    return value.length === 9;
+  }
+
+  return (
+    <TextField
+      label='Resident ID'
+      value={residentIdState?.value}
+      error={error ? 'Please provide a valid ID' : false}
+      onChange={handleFieldChange}
+    />
+  );
 
 }
