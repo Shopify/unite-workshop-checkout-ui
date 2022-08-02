144fd5874adfc17d4852d5b0a21b660413e1a9c3
Elizabeth Kenyon <elizabeth.kenyon@shopify.com>
2022-08-02 11:14:45 -0500

    Checkout UI Extension
    This branch represents step four in the workshop
    
    Use metafield as state
    
    Save resident ID as metafield on order

--- extensions/resident-id/src/index.jsx
+++ extensions/resident-id/src/index.jsx
@@ -1,23 +1,38 @@
 import React, { useState } from 'react';
-import  {render, TextField } from '@shopify/checkout-ui-extensions-react';
+import { render, useMetafield, TextField, useApplyMetafieldsChange } from '@shopify/checkout-ui-extensions-react';
 
 render('Checkout::Dynamic::Render', () => <App />);
 
 function App() {
+  const METAFIELD_NAMESPACE = 'RESIDENT_ID_APP';
+  const METAFIELD_KEY = 'resident_id';
+
   const [error, setError] = useState(false);
-  const [residentID, setResidentID] = useState('');
+  const updateMetafield = useApplyMetafieldsChange();
 
-  handleFieldChange = (value) => {
-    setResidentID(value);
-  };
+  const residentIdState = useMetafield({
+    namespace: METAFIELD_NAMESPACE,
+    key: METAFIELD_KEY,
+  });
 
-    return (
-      <TextField 
-        label='Resident ID'
-        value={residentID}
-        error={error? 'Please provide a valid ID': false}
-        onChange={handleFieldChange}
-         />
-      );
+  const handleFieldChange = (value) => {
+    updateMetafield(
+         {
+           type: 'updateMetafield',
+           namespace: METAFIELD_NAMESPACE,
+           key: METAFIELD_KEY,
+           valueType: 'string',
+           value: value
+         }
+       );
+   };
 
+  return (
+    <TextField 
+      label='Resident ID'
+      value={residentIdState?.value}
+      error={error? 'error': false}
+      onChange={handleFieldChange}
+     />
+  );
 }
